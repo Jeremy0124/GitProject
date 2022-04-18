b5aa346c6b8cbf019f95eac94cdfcfcfe9129e94
Jeremy0124 <Jeremy.0124@yahoo.com>
2022-04-18 09:47:09 -0600

    Initial Commit

---
+++ GitProject/GitProject/GitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitProjectApp.swift
+//  GitProject
+//
+//  Created by Jeremy Jackman on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct GitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
